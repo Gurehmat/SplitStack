4636deacc0fd0b91bdcb141eedf81418559df835
Gurehmat <gurehmat9@gmailcom>
2025-07-09 11:21:59 -0400

    some javascript logic

--- script.js
+++ script.js
@@ -5,7 +5,36 @@ function addPerson() {
     let name = document.getElementById("person-name").value;
     if (name) {
         people.push(name);
+        addpersontolist();
         console.log(people);
     }
+
+    document.getElementById("person-name").value = "";
+
+    
+
 }
 
+
+
+function addpersontolist() {
+    
+    let payers = document.getElementById("paid-by");
+
+    let splitters = document.getElementById("split-between");
+    
+    payers.innerHTML = "";
+
+    splitters.innerHTML = "";
+
+
+     for (let i = 0; i < people.length; i++) {
+        let opt = document.createElement("option");
+        opt.value = people[i];
+        opt.textContent = people[i];
+        
+        payers.appendChild(opt);
+        splitters.appendChild(opt.cloneNode(true));
+    }
+
+}
